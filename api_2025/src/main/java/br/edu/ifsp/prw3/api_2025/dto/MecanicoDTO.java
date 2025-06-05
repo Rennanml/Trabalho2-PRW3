d82498ceb8a0dae939019555e7a9c1be188ab769
Rennan Marcile Lazarini <rennanml112@gmail.com>
2025-06-04 23:01:58 -0300

    fix: wrong binding at table columns

--- api_2025/src/main/java/br/edu/ifsp/prw3/api_2025/dto/MecanicoDTO.java
+++ api_2025/src/main/java/br/edu/ifsp/prw3/api_2025/dto/MecanicoDTO.java
@@ -1,7 +1,6 @@
 package br.edu.ifsp.prw3.api_2025.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record MecanicoDTO(
         String codigo,
